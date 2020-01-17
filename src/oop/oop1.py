8ca08d5fba7990fd5cb759315bfc2a3f9a4dc6fe
Ben <bigriffin94@outlook.com>
2020-01-16 17:36:25 -0800

    oop1.py complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,12 +1,12 @@
 # Write classes for the following class hierarchy:
 #
-#  [Vehicle]->[FlightVehicle]->[Starship]
+#  [Vehicle]->[FlightVehicle]->[Starship] ALL PARENT CLASSES OR SUPER CLASSES
 #      |                |
 #      v                v
-# [GroundVehicle]      [Airplane]
+# [GroundVehicle]      [Airplane] CHILDREN/SUB CLASSES OF VEHICLES/STARSHIPS
 #   |       |
 #   v       v
-# [Car]  [Motorcycle]
+# [Car]  [Motorcycle] CHILD/SUBCLASSES OF GROUND VEHICLE ONLY
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
@@ -17,3 +17,41 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle and its subclasses BEGIN
+class Vehicle:
+    '''Parent Class to GroundVehicle and Grandaprent Class to Car Class'''
+    pass
+
+class GroundVehicle(Vehicle):
+    '''GV extends Vehicle, its only parent. Takes all attrs of Vehicle'''
+    pass
+
+class Car(GroundVehicle):
+    '''Car extends GV and takes all of GV + Vehicle attrs'''
+    pass
+
+class Motorcycle(GroundVehicle):
+    '''Motorcycle extends GV and takes all of GV + Vehicle attrs'''
+    pass
+# Vehicle and its subclasses END
+################################
+
+# FlightVehicle and its subclass BEGIN
+class FlightVehicle:
+    '''Parent Class to Airplane class only.'''
+    pass
+
+class Airplane(FlightVehicle):
+    '''Airplane Class extends Airplane class and takes all airplane attrs.'''
+    pass
+
+# FlightVehicle and its subclass END
+################################
+
+# Starship is a stand alone class. NO subclasses 
+class Starship:
+    '''Parent Class to nothing.'''
+    pass
+
+
